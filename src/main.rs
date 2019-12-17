7f8355d03338d3999266940b7b5db7e92fbfac51
Kristofer Bengtsson <kristofer@sekvensa.se>
2019-12-17 14:02:32 +0100

    added extra example

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@ use async_std::*;
 use prelude::*;
 use task;
 use sync::channel;
-use sync::{Receiver};
-use stream::Stream;
-use pin::Pin;
-use pin_project_lite::pin_project;
+
+use std::collections::hash_map::HashMap;
+
+mod stream_buffer;
+use stream_buffer::ChannelMerger;
 
 
 fn main() {
-    let (tx, rx) = channel(10);
+    // Simple example when sending usize and adding when item is not 0
+    let (tx, rx) = channel(2);
     let mut merger = ChannelMerger::new(
         rx,
         |x: &mut usize, y: &usize| {  // merging until we get a 0
@@ -19,8 +21,17 @@ fn main() {
         }
     );
 
+    // Example when struct has a hashmap that will always merge
+    let (tx2, rx2) = channel(2);
+    let mut merger2 = ChannelMerger::new(
+        rx2,
+        |x: &mut MyState, y: &MyState| {  // merging until we get a 0
+            x.xs.extend(y.xs.iter().map(|(x, y)| (x.clone(), y.clone())));
+            true
+        }
+    );
 
-
+    // Sening usize
     task::spawn( async move { 
         for x in 1..10 {
             tx.send(x).await
@@ -31,85 +42,45 @@ fn main() {
         tx.send(0).await;
         tx.send(5).await;
         tx.send(6).await;
-        std::println!("Everything is sent");
+        std::println!("usize is sent");
     });
 
-    task::block_on(async move {
+
+    // sending MyState
+    task::spawn( async move { 
+        for x in 1..10 {
+            let mut m = HashMap::new();
+            m.insert(x.to_string(), "hej".to_string());
+            let state = MyState{xs: m};
+            tx2.send(state).await
+        }
+        std::println!("MyState is sent");
+    });
+
+    // pulling usize from buffer
+    let res = task::spawn(async move {
         while let Some(x) = merger.next().await {
             std::println!("GOT: {:?}", x);
         }
     });
-}
 
-pin_project! {
-    /// ChannelMerger is a stream buffer that will fold messages waiting in 
-    /// the incoming channel until the channel is empty or if the merge fn
-    /// returns false. This will happen for each call to next. 
-    /// 
-    /// Implement a folding function that merges the 
-    /// items and return true if an item was merged with previous. 
-    /// If you do not want to merge two items, just return false
-    /// 
-    /// # Example
-    /// ```
-    /// let (tx, rx) = channel(2);
-    /// let mut merger = ChannelMerger::new(
-    ///     rx,
-    ///     |x: &mut usize, y: &usize| {  // merging until we get a 0
-    ///         *x += y;
-    ///         *x != 0
-    ///     }
-    /// );
-    /// 
-    /// task::block_on(async move {
-    /// while let Some(x) = merger.next().await {
-    ///    std::println!("GOT: {:?}", x);
-    /// }
-    /// });
-    /// 
-    /// ```
-    ///
-    pub struct ChannelMerger<T, F> where F: FnMut(&mut T, &T) -> bool {
-        #[pin] rx: Receiver<T>,
-        merger: F, 
-        buffer: Option<T>,
-    }
-}
-
-impl<T, F> ChannelMerger<T, F> where F: FnMut(&mut T, &T) -> bool {
-    fn new(rx: Receiver<T>, merger: F) -> ChannelMerger<T, F> {
-        ChannelMerger{
-            rx, 
-            merger,
-            buffer: None,
+    // pulling states from buffer
+    let res2 = task::spawn(async move {
+        while let Some(x) = merger2.next().await {
+            std::println!("GOT: {:?}", x);
         }
-    }
+    });
+
+    task::block_on(async move {
+        res.await;
+        res2.await;
+    })
+
 }
 
-impl<T, F> Stream for ChannelMerger<T, F>  where T: Clone, F: FnMut(&mut T, &T) -> bool {
-    type Item = T;
-
-    fn poll_next(self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> task::Poll<Option<Self::Item>> {
-        let this = self.project();
-        loop {
-            if this.rx.is_empty() && this.buffer.is_none() {
-                return this.rx.poll_next(cx);
-            } else if this.rx.is_empty() && this.buffer.is_some() {
-                let temp = this.buffer.clone();
-                *this.buffer = None;
-                return task::Poll::Ready(temp);
-            } else {
-                let x = task::block_on(this.rx.recv()).unwrap();
-                if this.buffer.is_none() {
-                    *this.buffer = Some(x);
-                } else if !(this.merger)(this.buffer.as_mut().unwrap(), &x) {
-                    let temp = this.buffer.clone();
-                    *this.buffer = Some(x);
-                    return task::Poll::Ready(temp);
-                }
-
-            }
-        }
-        
-    }
+#[derive(Clone, Debug)]
+struct MyState {
+    xs: std::collections::hash_map::HashMap<String, String>
 }
+
+
---
+++ src/stream_buffer.rs
@@ -0,0 +1,78 @@
+use async_std::*;
+use sync::{Receiver};
+use stream::Stream;
+use pin::Pin;
+use pin_project_lite::pin_project;
+
+pin_project! {
+    /// ChannelMerger is a stream buffer that will fold messages waiting in 
+    /// the incoming channel until the channel is empty or if the merge fn
+    /// returns false. This will happen for each call to next. 
+    /// 
+    /// Implement a folding function that merges the 
+    /// items and return true if an item was merged with previous. 
+    /// If you do not want to merge two items, just return false
+    /// 
+    /// # Example
+    /// ```
+    /// let (tx, rx) = channel(2);
+    /// let mut merger = ChannelMerger::new(
+    ///     rx,
+    ///     |x: &mut usize, y: &usize| {  // merging until we get a 0
+    ///         *x += y;
+    ///         *x != 0
+    ///     }
+    /// );
+    /// 
+    /// task::block_on(async move {
+    /// while let Some(x) = merger.next().await {
+    ///    std::println!("GOT: {:?}", x);
+    /// }
+    /// });
+    /// 
+    /// ```
+    ///
+    pub struct ChannelMerger<T, F> where F: FnMut(&mut T, &T) -> bool {
+        #[pin] rx: Receiver<T>,
+        merger: F, 
+        buffer: Option<T>,
+    }
+}
+
+impl<T, F> ChannelMerger<T, F> where F: FnMut(&mut T, &T) -> bool {
+    pub fn new(rx: Receiver<T>, merger: F) -> ChannelMerger<T, F> {
+        ChannelMerger{
+            rx, 
+            merger,
+            buffer: None,
+        }
+    }
+}
+
+impl<T, F> Stream for ChannelMerger<T, F>  where T: Clone, F: FnMut(&mut T, &T) -> bool {
+    type Item = T;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> task::Poll<Option<Self::Item>> {
+        let this = self.project();
+        loop {
+            if this.rx.is_empty() && this.buffer.is_none() {
+                return this.rx.poll_next(cx);
+            } else if this.rx.is_empty() && this.buffer.is_some() {
+                let temp = this.buffer.clone();
+                *this.buffer = None;
+                return task::Poll::Ready(temp);
+            } else {
+                let x = task::block_on(this.rx.recv()).unwrap();
+                if this.buffer.is_none() {
+                    *this.buffer = Some(x);
+                } else if !(this.merger)(this.buffer.as_mut().unwrap(), &x) {
+                    let temp = this.buffer.clone();
+                    *this.buffer = Some(x);
+                    return task::Poll::Ready(temp);
+                }
+
+            }
+        }
+        
+    }
+}
